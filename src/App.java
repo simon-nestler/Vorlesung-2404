2f6ecce49930e25b2afbfee0b46dcda0a1348e99
Simon Nestler <simon.nestler@thi.de>
2025-04-24 11:50:50 +0200

    bubble sort - sortieren bis es nichts mehr zu sortieren gibt

--- src/App.java
+++ src/App.java
@@ -16,10 +16,22 @@ public class App {
 
         System.out.println(liste);
         bubbleSort(liste);
-        System.out.println(liste);
     }
 
     public static void bubbleSort(ArrayList<Integer> liste) {
+        boolean swap = true;
 
+        while (swap) {
+            swap = false;
+            for (int i = 0; i < liste.size() - 1; i++) {
+                if (liste.get(i) > liste.get(i + 1)) {
+                    int temp = liste.get(i);
+                    liste.set(i, liste.get(i + 1));
+                    liste.set(i + 1, temp);
+                    swap = true;
+                }
+            }
+            System.out.println(liste);
+        }
     }
 }
