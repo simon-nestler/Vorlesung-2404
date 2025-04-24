522e2df1d400f80e19728338e1cab14c455f8499
Simon Nestler <simon.nestler@thi.de>
2025-04-24 11:33:58 +0200

    liste, bubble sort noch nicht implementiert.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Integer> liste = new ArrayList<Integer>();
+        liste.add(4);
+        liste.add(5);
+        liste.add(4);
+        liste.add(0);
+        liste.add(8);
+        liste.add(2);
+        liste.add(3);
+        liste.add(7);
+        liste.add(1);
+        liste.add(6);
+
+        System.out.println(liste);
+        bubbleSort(liste);
+        System.out.println(liste);
+    }
+
+    public static void bubbleSort(ArrayList<Integer> liste) {
+
     }
 }
